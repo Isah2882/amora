86dfd281c4d18157c3bbead0925c6f9b4c34ebbe
Isah2882 <conceicaodesouzaisadora@gmail.com>
2025-06-16 11:18:53 -0300

    react navigation

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens.jsx/home";
+import DetailsScreen from "./screens.jsx/details";
+import ProfileScreen from "./screens.jsx/profile";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack =createStackNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+            name="Home"
+            component={HomeScreen} 
+            options={{title: 'Tela Principal', headerStyle:{backgroundColor:'#007bff'},
+            headerTintColor:'#fff'}}/>
+            <Stack.Screen 
+            name="Details"
+            component={DetailsScreen} 
+            options={{title: 'Detalhes', headerStyle:{backgroundColor:'#dc3545'},
+            headerTintColor:'#fff'}}/>
+            <Stack.Screen 
+            name="Profile"
+            component={ProfileScreen}
+            options={{title: 'Perfil', headerStyle:{backgroundColor:'#ba55d3'},
+            headerTintColor:'#fff'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
